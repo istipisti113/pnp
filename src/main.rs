3a3fe205cf45b04b111f4ceed057df0fa5b05901
Szabo Istvan <pisti4395@gmail.com>
2025-10-15 22:36:21 +0200

    css tinker

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 use warp::{Filter};
+//use std::sync::{Arc, Mutex};
 
 #[tokio::main]
 async fn main() {
     let port = 4040;
-    let home = warp::path::end().map(||{});
+    println!("running on port {}",port);
 
-    let routes = home;
+    let style = warp::path("style.css").and(warp::fs::file("style.css"));
+    let bootstrapcss = warp::path("bootstrap.css").and(warp::fs::file("bootstrap.css"));
+    let bootstrapjs = warp::path("bootstrap.js").and(warp::fs::file("bootstrap.js"));
+    let home = warp::path::end().map(||{
+        warp::reply::html(std::fs::read("index.html").unwrap())
+    });
+
+    let routes = home.or(style).or(bootstrapcss);
     warp::serve(routes).run(([0,0,0,0], port)).await;
 }
